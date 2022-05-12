fe9431f20856b8795f222916fb113f8fef407e4e
Siyuan sharon Li <lucky8@MacBook-Pro.ht.home>
2022-05-12 14:32:27 -0400

    new code

--- snake.py
+++ snake.py
@@ -1,22 +1,62 @@
 import pygame
 import sys #system functions, e.g. quit(stop running code)
+import random
+from pygame.math import Vector2
+
+class SNAKE:
+    def __init__(self):
+        self.body = [Vector2(5,10),Vector2(6,10),Vector2(7,10)]
+        self.direction = Vector2(1,0)
+
+    def draw_snake(self):
+        for block in self.body:
+            x_pos = int(block.x * cell_size)
+            y_pos = int(block.y * cell_size)
+            snake_rect = pygame.Rect(x_pos,y_pos,cell_size,cell_size)
+            pygame.draw.rect(screen,(0,0,225),snake_rect)
+    def move_snake(self):
+        body_copy = self.body[:-1]
+        body_copy.insert(0,body_copy[0]+self.direction)
+        self.body = body_copy[:]
+ 
+class FRUIT: 
+    def __init__(self):
+        self.x = random.randint(0,cell_number-1)
+        self.y = random.randint(0,cell_number-1)
+        self.pos = Vector2(self.x,self.y)
+
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size),int(self.pos.y * cell_size),cell_size,cell_size)
+        pygame.draw.rect(screen,(220,166,114),fruit_rect)
+
 
 pygame.init()
+cell_size = 40
+cell_number = 20
+screen_width = cell_size * cell_number
 
 #initailize the display surface
-screen = pygame.display.set_mode((400,500))
+screen = pygame.display.set_mode((screen_width,screen_width))
 #initialize a clock and can later set to different parameter
 #for purpose of uniforming running speed of game
 #if tick(60), loop 60 times in one min
 clock = pygame.time.Clock()
-test_rect = pygame.Rect(100,200,100,100)
+
+fruit = FRUIT()
+snake = SNAKE()
+
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE,150)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == SCREEN_UPDATE:
+            snake.move_snake()
     screen.fill((74,139,48))
-    pygame.draw.rect(screen,pygame.Color('red'),test_rect)
+    fruit.draw_fruit()
+    snake.draw_snake()
     pygame.display.update()
     clock.tick(60)   
